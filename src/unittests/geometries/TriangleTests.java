71c388d9741f3be2b883d252eff535323657de55
יעל אוטמזגין <yotmazgi@g.jct.ac.il>
2023-04-20 10:32:30 +0300

    Geometries

---
+++ src/unittests/geometries/TriangleTests.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package unittests.geometries;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import primitives.*;
+import geometries.*;
+
+/**
+ * @author Dell
+ *
+ */
+class TriangleTests {
+
+	/**
+	 * Test method for {@link geometries.Triangle#getNormal(primitives.Point)}.
+	 */
+	@Test
+	void testGetNormal() {
+		//Inherits from the polygon
+		fail("Not yet implemented");
+	}
+
+}
